e0e4c30802b8269e6ad65a19a518da412e662264
PopovaAnna1209 <popovaanna120987@gmail.com>
2023-07-04 01:58:26 +0300

    commit2

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,15 +1,17 @@
-import { RotatingLines } from 'react-loader-spinner';
+import { Hearts } from 'react-loader-spinner';
 import s from './Loader.module.css';
 
 export const Loader = () => {
   return (
     <div className={s.loading}>
-      <RotatingLines
-        strokeColor="rgba(178 146 171 / 50%)"
-        strokeWidth="5"
-        animationDuration="0.75"
-        width="50"
-        visible={true}
+      <Hearts 
+          height="100"
+          width="100"
+          color="rgba(178, 146, 171, 0.742)"
+          ariaLabel="hearts-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
       />
     </div>
   );
